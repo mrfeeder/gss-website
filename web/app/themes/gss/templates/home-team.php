dbdb23ad6236c3bcf4b05d99a60b2e9b2e6ef79f
mrfeeder <thanh.tran@gsmartsolutions.com>
2017-05-17 10:52:10 +0700

    create our team part in hompage

--- web/app/themes/gss/templates/home-team.php
+++ web/app/themes/gss/templates/home-team.php
@@ -1,1 +1,45 @@
-<h1>Our Team</h1>
+<div class="our-team">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 our-services-top">
+                <p class="lead text-center">Meet People</p>
+                <h1 class="text-center text-uppercase">OUR teams</h1>
+            </div>
+        </div>
+    </div>
+    <div class="container team-blocks">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="team-block">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/user_placeholder.png" alt="" class="img-responsive">
+                    <h3>John Doe</h3>
+                    <p>Developer</p>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="team-block">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/user_placeholder.png" alt="" class="img-responsive">
+                    <h3>John Doe</h3>
+                    <p>Developer</p>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="team-block">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/user_placeholder.png" alt="" class="img-responsive">
+                    <h3>John Doe</h3>
+                    <p>Developer</p>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="team-block">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/user_placeholder.png" alt="" class="img-responsive">
+                    <h3>John Doe</h3>
+                    <p>Developer</p>
+                </div>
+            </div>
+        </div>
+        <div class="row area-show-more">
+            <div class="col-md-12"><a href="#" class="btn btn-outline-primary">all member</a></div>
+        </div>
+    </div>
+</div>
