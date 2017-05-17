8ac7ff0bfd59d3d077d35cea137dcfd3c5af4909
mrfeeder <thanh.tran@gsmartsolutions.com>
2017-05-17 10:25:25 +0700

    create our work in home page

--- web/app/themes/gss/templates/home-works.php
+++ web/app/themes/gss/templates/home-works.php
@@ -1,1 +1,53 @@
-<h1>Our Works</h1>
+<div class="our-work">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 our-services-top">
+                <p class="lead text-center">Work We Did</p>
+                <h1 class="text-center text-uppercase">OUR WORK</h1>
+            </div>
+        </div>
+    </div>
+    <nav class="navbar navbar-expand-md navbar-light bg-faded text-center">
+        <div class="container">
+            <div class="collapse navbar-collapse text-center justify-content-center" id="navbar2SupportedContent">
+                <ul class="navbar-nav">
+                    <li class="nav-item active"> <a class="nav-link" href="#">All</a> </li>
+                    <li class="nav-item"> <a class="nav-link" href="#">Branding</a> </li>
+                    <li class="nav-item"> <a class="nav-link" href="#">Website</a> </li>
+                    <li class="nav-item"> <a class="nav-link" href="#">Mobile</a> </li>
+                    <li class="nav-item"><a class="nav-link" href="#">Outsource</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="container-fluid block-items">
+        <div class="row">
+            <div class="col-md-3 block-item">
+                <img class="center-block img-fluid d-block" src="<?php echo get_template_directory_uri(); ?>/assets/images/image.png" alt="">
+            </div>
+            <div class="col-md-3 block-item">
+                <img class="center-block img-fluid d-block" src="<?php echo get_template_directory_uri(); ?>/assets/images/agentcy.png" alt="">
+            </div>
+            <div class="col-md-3 block-item">
+                <img class="center-block img-fluid d-block" src="<?php echo get_template_directory_uri(); ?>/assets/images/book.png" alt="">
+            </div>
+            <div class="col-md-3 block-item">
+                <img class="center-block img-fluid d-block" src="<?php echo get_template_directory_uri(); ?>/assets/images/girl.png" alt="">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3 block-item">
+                <img class="center-block img-fluid d-block" src="<?php echo get_template_directory_uri(); ?>/assets/images/threebox.png" alt="">
+            </div>
+            <div class="col-md-3 block-item">
+                <img class="center-block img-fluid d-block" src="<?php echo get_template_directory_uri(); ?>/assets/images/number8.png" alt="">
+            </div>
+            <div class="col-md-3 block-item">
+                <img class="center-block img-fluid d-block" src="<?php echo get_template_directory_uri(); ?>/assets/images/fastion.png" alt="">
+            </div>
+            <div class="col-md-3 block-item">
+                <img class="center-block img-fluid d-block" src="<?php echo get_template_directory_uri(); ?>/assets/images/box.png" alt="">
+            </div>
+        </div>
+    </div>
+</div>
