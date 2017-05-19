c5458ec63af1c5895d630b83e1ea0ca64f1fbc35
mrfeeder <thanh.tran@gsmartsolutions.com>
2017-05-19 09:39:30 +0700

    create template home static part

--- web/app/themes/gss/templates/home-statistics.php
+++ web/app/themes/gss/templates/home-statistics.php
@@ -1,1 +1,49 @@
-<h1>Number statistics</h1>
+<div class="we-have">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-2">
+                <div class="border-image">
+                    <img class="img-responsive" alt="" src="<?php echo get_template_directory_uri(); ?>/assets/images/Shape-1.png">
+                </div>
+                <h1>08</h1>
+                <p>Years experience</p>
+            </div>
+            <div class="col-md-2">
+                <div class="border-image">
+                    <img class="img-responsive" alt="" src="<?php echo get_template_directory_uri(); ?>/assets/images/Shape-2.png">
+                </div>
+                <h1>69</h1>
+                <p>Team member</p>
+            </div>
+            <div class="col-md-2">
+                <div class="border-image">
+                    <img class="img-responsive" alt="" src="<?php echo get_template_directory_uri(); ?>/assets/images/Shape-3.png">
+                </div>
+                <h1>457</h1>
+                <p>coffes drank</p>
+            </div>
+            <div class="col-md-2">
+                <div class="border-image">
+                    <img class="img-responsive" alt="" src="<?php echo get_template_directory_uri(); ?>/assets/images/Shape-4.png">
+                </div>
+                <h1>13</h1>
+                <p>Happy clients</p>
+            </div>
+            <div class="col-md-2">
+                <div class="border-image">
+                    <img class="img-responsive" alt="" src="<?php echo get_template_directory_uri(); ?>/assets/images/Shape-5.png">
+                </div>
+                <h1>70</h1>
+                <p>Projects</p>
+            </div>
+            <div class="col-md-2">
+                <div class="border-image">
+                    <img class="img-responsive" alt="" src="<?php echo get_template_directory_uri(); ?>/assets/images/Shape-6.png">
+                </div>
+                <h1>90m</h1>
+                <p>Lines of codes</p>
+            </div>
+        </div>
+    </div>
+</div>
+
