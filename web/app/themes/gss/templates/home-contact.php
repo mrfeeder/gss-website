7fca17f2b4d226e180bd59912e2710b82a68583d
mrfeeder <thanh.tran@gsmartsolutions.com>
2017-05-19 10:32:52 +0700

    create template contact us home page

--- web/app/themes/gss/templates/home-contact.php
+++ web/app/themes/gss/templates/home-contact.php
@@ -1,1 +1,38 @@
-<h1>Contact us</h1>
+<div class="contact-us">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 intro-contact">
+                <h3 class="text-uppercase">We can help you?</h3>
+                <p>Photographs are a way of preserving a moment in our lives for the rest of our lives. Many of us have at least been tempted at the flashy array of photo</p>
+                <p>
+                    <span class="sup-mail">
+                        support@gss.com.vn
+                    </span>
+                    <span class="sup-skype fa fa-skype">
+                        gssskype
+                    </span>
+                    <span class="sup-phone">
+                        +84 123 456 7890
+                    </span>
+                </p>
+            </div>
+            <div class="col-md-5 col-md-offset-1 form-contact">
+                <form class="text-center">
+                    <fieldset class="form-group">
+                        <input type="text" class="form-control" id="exampleInputEmail1" placeholder="name" name="name">
+                    </fieldset>
+                    <fieldset class="form-group">
+                        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="email" name="email">
+                    </fieldset>
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="phone" name="phone">
+                    </div>
+                    <div class="form-group">
+                        <textarea class="form-control" id="exampleTextarea" rows="3" placeholder="message" name="message"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-outline-primary">send message</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
