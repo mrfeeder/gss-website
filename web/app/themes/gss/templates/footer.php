86f043c7d4e71ece86918d23f2ca359777cace09
mrfeeder <thanh.tran@gsmartsolutions.com>
2017-05-19 11:22:31 +0700

    create footer for page

--- web/app/themes/gss/templates/footer.php
+++ web/app/themes/gss/templates/footer.php
@@ -1,5 +1,50 @@
 <footer>
     <div class="container">
-        <?php dynamic_sidebar('sidebar-footer'); ?>
+        <div class="row">
+            <div class="col-md-4 add-company">
+                <img class="img-responsive" alt="" src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-footer.png">
+                <p>Photographs are a way preserving a moment in our lives for the rest of our lives</p>
+                <p class="position-company">6081 Jovanny Shoals Suite 236, Ha Noi, Viet Nam</p>
+            </div>
+            <div class="col-md-4 menu-footer">
+                <h3>Quick link</h3>
+                <ul>
+                    <li><a href="#">Home page</a></li>
+                    <li><a href="#">What we are</a></li>
+                    <li><a href="#">What we do</a></li>
+                </ul>
+                <ul>
+                    <li><a href="#">Out client</a></li>
+                    <li><a href="#">Contact us</a></li>
+                    <li><a href="#">Get a quote</a></li>
+                </ul>
+            </div>
+            <div class="col-md-4 newsletter">
+              <h3>newsletter</h3>
+                <form class="form-inline">
+                    <div class="form-group">
+                        <input type="email" class="form-control" placeholder="Enter email">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Subscribe</button>
+                </form>
+                <ul class="icon-menu">
+                    <li class="face-icon">FACEBOOK</li>
+                    <li class="google-icon">GOOGLE</li>
+                    <li class="twiter-icon">TWITER</li>
+                    <li class="insta-icon">INSTAGRAM</li>
+                </ul>
+            </div>
+        </div>
     </div>
 </footer>
+<div class="coppy-right">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <p>
+                    COPPYRIGHT 2017 &#169; GLOBAL SMART SOLUTION
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
